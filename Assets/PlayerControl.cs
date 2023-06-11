ae7d59c7cd5b8152758d2c51279cda4fda1a4798
Vlad Ch <vladchistov13@gmail.com>
2023-06-11 19:00:40 +0300

    fourth commit

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
 
 
-    //public CharacterController2D controller;
-    //public Animatior animatior;
+    public CharacterController Controller;
+    public Animator Animator;
 
-
+    
     public Transform groundcheck;
     public LayerMask groundMask;
     private bool ground = false;
     private float groundRadius = 0.5f;
 
-    
+    public int score;
+    public Text scoreT;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -31,8 +35,29 @@ public class PlayerControl : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && ground==true)
         {
             Jump();
+            Animator.SetBool("jumping", true);
+        }
+
+        else
+        {
+            Animator.SetBool("jumping", false);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            SceneManager.LoadScene(2);
+        }
+        //if (collision.tag == "Score")
+        //{
+        //    score++;
+        //    scoreT.text = score.ToString();
+        //}
+    }
+
+
 
     public void Jump()
     {
